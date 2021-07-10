152dad6e9226f9637eaa9e64424a165a8f622dd4
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2021-07-10 18:24:04 +0300

    Fixed user states, .ogg to .wav converting, added topics and languages

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
+import os
+import subprocess
+import time
+from collections import defaultdict
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import ParseMode
 from aiogram.utils import executor
-from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
+
 import db_controller
-import firebase_admin
-import subprocess
-import os
-import vosk
-from utils import days_ago_date, process_fetched, get_date, date_to_timestamp
+from bot_kb import get_rec_kb, get_lan_kb, freq_used_topics_kb, freq_used_dates_kb
 from recognize_controller import recognize_phrase
+from utils import days_ago_date, process_fetched, get_date, date_to_timestamp, process_text, format_recording
 
 TOKEN = 'TOKEN'
-
-VOICE_MODEL = vosk.Model
-
-if not firebase_admin._apps:
-    cred = firebase_admin.credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
+# TODO CONVERT STRING STATES TO ENUM
 """
     POSSIBLE STATES:
     - IDLE
-    - VID_PR - video processing
+    - AUD_INP_TP - audio processing (getting topic)
+    - AUD_INP_LN - audio processing (getting language input)
+    - AUD_PR - audio processing (downloading, converting, storing)
     - GET_ALL - get all the recordings
     - GET_LAST_N - get last N recordings
     - GET_BY_DATE - get all by date
@@ -32,161 +33,213 @@ dp = Dispatcher(bot)
     - GET_ALL_AF - get all after date
     - GET_ALL_TP - get all by topic
 """
-STATES = dict()
-
-get_rec_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)\
-    .add(KeyboardButton('Get all the recordings'))\
-    .row(KeyboardButton('by date'), KeyboardButton('by topic'))\
-    .add(KeyboardButton('last N recordings'))\
-    .row(KeyboardButton('between two dates'), KeyboardButton('after date'))
-
-freq_used_dates_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)\
-    .add(KeyboardButton('Today')).add(KeyboardButton('Yesterday')).add(KeyboardButton('Past week'))
-
-freq_used_topics_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton('None'))
+USER_DATA = defaultdict(defaultdict)
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(msg: types.Message):
-    STATES[msg.from_user.id] = 'IDLE'
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
     await msg.reply('Hello there!\nI can recognize phrases from your voice messages, convert them to txt, '
-                    'and store in Firestore DB. Send me a voice message to start.', reply_markup=get_rec_kb)
+                    'and store in Firestore DB. Send me a voice message to start. I support messages in '
+                    '<b>English</b>, <b>Russian</b>, and <b>Ukrainian.</b>',
+                    reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
 
 
-@dp.message_handler(text=['Get all the recordings', 'Отримати всі', 'Получить все'])   # TODO LOCAL
+@dp.message_handler(text=['Get all the recordings'])
 async def process_get_all(msg: types.Message):
-    STATES[msg.from_user.id] = 'GET_ALL'
-    recordings = process_fetched(await db_controller.fetch_all(user_id=msg.from_user.id))
-    await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-    STATES[msg.from_user.id] = 'IDLE'
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        USER_DATA[msg.from_user.id]['state'] = 'GET_ALL'
+        recordings = process_fetched(await db_controller.fetch_all(user_id=msg.from_user.id))
+        for recording in recordings:
+            await msg.reply(format_recording(recording=recording), reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
-@dp.message_handler(text=['by date', 'after date'])   # TODO LOCAL
+@dp.message_handler(text=['by date', 'after date'])
 async def process_get_by_date(msg: types.Message):
-    STATES[msg.from_user.id] = 'GET_BY_DATE' if msg.text == 'Get by date' else 'GET_ALL_AF'
-    await msg.reply('Send me a date in format <b>YYYY-mm-dd HH:MM:SS</b> or just <b>YYYY-mm-dd</b>.',
-                    reply_markup=freq_used_dates_kb, parse_mode=ParseMode.HTML)
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        USER_DATA[msg.from_user.id]['state'] = 'GET_BY_DATE' if msg.text == 'by date' else 'GET_ALL_AF'
+        await msg.reply('Send me a date in format <b>YYYY-mm-dd HH:MM:SS</b> or just <b>YYYY-mm-dd</b>.',
+                        reply_markup=freq_used_dates_kb, parse_mode=ParseMode.HTML)
 
 
-@dp.message_handler(text=['Today', 'Yesterday', 'Past week'])   # TODO LOCAL
+@dp.message_handler(text=['Today', 'Yesterday', 'Past week'])
 async def process_get_date_kb(msg: types.Message):
-    if STATES[msg.from_user.id] in ['GET_BY_DATE', 'GET_ALL_AF']:
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] in ['GET_BY_DATE', 'GET_ALL_AF']:
         cases = {'Today': 0, 'Yesterday': 1, 'Past week': 7}
         datestamp = days_ago_date(days=cases[msg.text])
         recordings = process_fetched(
             await db_controller.fetch_by_date(user_id=msg.from_user.id, date=datestamp, is_exact=False)
-            if STATES[msg.from_user.id] == 'GET_BY_DATE' else
+            if USER_DATA[msg.from_user.id]['state'] == 'GET_BY_DATE' else
             await db_controller.fetch_after(user_id=msg.from_user.id, date=datestamp)
         )
-        await msg.reply(str(recordings), reply_markup=get_rec_kb)  # TODO NICE VISUALIZATION
-        STATES[msg.from_user.id] = 'IDLE'
+        for recording in recordings:
+            await msg.reply(format_recording(recording=recording), reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
-@dp.message_handler(text=['between two dates'])   # TODO LOCAL
+@dp.message_handler(text=['between two dates'])
 async def process_get_between(msg: types.Message):
-    STATES[msg.from_user.id] = 'GET_ALL_BW'
-    await msg.reply('Send me two dates separated by space in format <b>YYYY-mm-dd HH:MM:SS YYYY-mm-dd '
-                    'HH:MM:SS</b> or just <b>YYYY-mm-dd YYYY-mm-dd</b>.', parse_mode=ParseMode.HTML)
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        USER_DATA[msg.from_user.id]['state'] = 'GET_ALL_BW'
+        await msg.reply('Send me two dates separated by space in format <b>YYYY-mm-dd HH:MM:SS YYYY-mm-dd '
+                        'HH:MM:SS</b> or just <b>YYYY-mm-dd YYYY-mm-dd</b>.', parse_mode=ParseMode.HTML)
 
 
 @dp.message_handler(regexp=r'^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2})?$')
 async def process_get_date_input(msg: types.Message):
-    # there are only two acceptable cases: either it's just a date or it's a date plus time
-    len_with_time = 19
-    len_without_time = 10
-    datestamp = msg.text
-    if STATES[msg.from_user.id] == 'GET_BY_DATE':
-        is_exact = True
-        if len(datestamp) == len_without_time:
-            datestamp = f'{datestamp} 00:00:00'
-            is_exact = False
-        print(datestamp)
-        recordings = process_fetched(
-            await db_controller.fetch_by_date(user_id=msg.from_user.id, date=datestamp, is_exact=is_exact)
-        )
-        await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-        STATES[msg.from_user.id] = 'IDLE'
-    elif STATES[msg.from_user.id] == 'GET_ALL_AF':
-        if len(datestamp) == len_without_time:
-            datestamp = f'{datestamp} 00:00:00'
-        if len(datestamp) == len_with_time:
-            recordings = process_fetched(await db_controller.fetch_after(user_id=msg.from_user.id, date=datestamp))
-            await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-        STATES[msg.from_user.id] = 'IDLE'
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        # there are only two acceptable cases: either it's just a date or it's a date plus time
+        len_with_time = 19
+        len_without_time = 10
+        datestamp = msg.text
+        if USER_DATA[msg.from_user.id]['state'] == 'GET_BY_DATE':
+            is_exact = True
+            if len(datestamp) == len_without_time:
+                datestamp = f'{datestamp} 00:00:00'
+                is_exact = False
+            recordings = process_fetched(
+                await db_controller.fetch_by_date(user_id=msg.from_user.id, date=datestamp, is_exact=is_exact)
+            )
+            for recording in recordings:
+                await msg.reply(format_recording(recording=recording),
+                                reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+            USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+        elif USER_DATA[msg.from_user.id]['state'] == 'GET_ALL_AF':
+            if len(datestamp) == len_without_time:
+                datestamp = f'{datestamp} 00:00:00'
+            if len(datestamp) == len_with_time:
+                recordings = process_fetched(await db_controller.fetch_after(user_id=msg.from_user.id, date=datestamp))
+                for recording in recordings:
+                    await msg.reply(format_recording(recording=recording),
+                                    reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+            USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
 @dp.message_handler(regexp=r'^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2})? \d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2})?$')
 async def process_get_between_input(msg: types.Message):
-    len_with_time = 39
-    len_without_time = 21
-    if STATES[msg.from_user.id] == 'GET_ALL_BW':
-        datestamps = msg.text
-        if len(datestamps) == len_without_time:
-            datestamps = f'{datestamps[:10]} 00:00:00 {datestamps[11:]} 23:59:59'
-        if len(datestamps) == len_with_time:
-            date1 = datestamps[:19]
-            date2 = datestamps[20:]
-            # get all the matches for this date
-            recordings = process_fetched(
-                await db_controller.fetch_between(user_id=msg.from_user.id, date1=date1, date2=date2)
-            )
-            await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-            STATES[msg.from_user.id] = 'IDLE'
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        len_with_time = 39
+        len_without_time = 21
+        if USER_DATA[msg.from_user.id]['state'] == 'GET_ALL_BW':
+            datestamps = msg.text
+            if len(datestamps) == len_without_time:
+                datestamps = f'{datestamps[:10]} 00:00:00 {datestamps[11:]} 23:59:59'
+            if len(datestamps) == len_with_time:
+                date1 = datestamps[:19]
+                date2 = datestamps[20:]
+                # get all the matches for this date
+                recordings = process_fetched(
+                    await db_controller.fetch_between(user_id=msg.from_user.id, date1=date1, date2=date2)
+                )
+                for recording in recordings:
+                    await msg.reply(format_recording(recording=recording),
+                                    reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+                USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
-@dp.message_handler(text=['last N recordings'])   # TODO LOCAL
+@dp.message_handler(text=['last N recordings'])
 async def process_get_last_n_command(msg: types.Message):
-    STATES[msg.from_user.id] = 'GET_LAST_N'
-    await msg.reply('Send me a number of recordings you want to get.')
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        USER_DATA[msg.from_user.id]['state'] = 'GET_LAST_N'
+        await msg.reply('Send me a number of recordings you want to get.')
 
 
-@dp.message_handler(regexp=r'^\d+$')   # TODO LOCAL
-async def process_get_last_n_number_input(msg: types.Message):
-    if STATES[msg.from_user.id] == 'GET_LAST_N':
+@dp.message_handler(regexp=r'^\d+$')
+async def process_get_number_input(msg: types.Message):
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] == 'GET_LAST_N':
         recordings = process_fetched(await db_controller.fetch_last_n(user_id=msg.from_user.id, number=int(msg.text)))
-        await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-        STATES[msg.from_user.id] = 'IDLE'
+        for recording in recordings:
+            await msg.reply(format_recording(recording=recording), reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
-@dp.message_handler(text=['by topic'])   # TODO LOCAL
+@dp.message_handler(text=['by topic'])
 async def process_get_by_topic_command(msg: types.Message):
-    STATES[msg.from_user.id] = 'GET_BY_TP'
-    await msg.reply('Send me a topic name to search for.', reply_markup=freq_used_topics_kb)
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        USER_DATA[msg.from_user.id]['state'] = 'GET_ALL_TP'
+        await msg.reply('Send me a topic name to search for.', reply_markup=freq_used_topics_kb)
 
 
-@dp.message_handler(regexp=r'^.*$')   # TODO LOCAL
-async def process_get_topic_input(msg: types.Message):
-    if STATES[msg.from_user.id] == 'GET_BY_TP':
+@dp.message_handler(regexp=r'^.*$')
+async def process_get_text_input(msg: types.Message):
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] == 'GET_ALL_TP':
         recordings = process_fetched(await db_controller.fetch_by_topic(user_id=msg.from_user.id, topic=msg.text))
-        await msg.reply(str(recordings), reply_markup=get_rec_kb)   # TODO NICE VISUALIZATION
-        STATES[msg.from_user.id] = 'IDLE'
+        for recording in recordings:
+            await msg.reply(format_recording(recording=recording), reply_markup=get_rec_kb, parse_mode=ParseMode.HTML)
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+
+    elif USER_DATA[msg.from_user.id]['state'] == 'AUD_INP_TP':
+        USER_DATA[msg.from_user.id]['recording']['topic'] = msg.text
+        USER_DATA[msg.from_user.id]['state'] = 'AUD_INP_LN'
+        await msg.reply('Now enter the language of your voice message.', reply_markup=get_lan_kb)
+
+    elif USER_DATA[msg.from_user.id]['state'] == 'AUD_INP_LN':
+        # buttons also contain flag emojis, so we need to remove them from msg.text
+        USER_DATA[msg.from_user.id]['recording']['language'] = msg.text[3:]
+        USER_DATA[msg.from_user.id]['state'] = 'AUD_PR'
+        await convert_voice_message(msg)
 
 
 @dp.message_handler(content_types=['voice'])
 async def process_voice_message(msg: types.Message):
-    datestamp = get_date()
-    timestamp = date_to_timestamp(datestamp)
-    filename = f'{msg.chat.id}_{timestamp}'
+    if 'state' not in USER_DATA[msg.from_user.id].keys():
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
+    if USER_DATA[msg.from_user.id]['state'] not in ['AUD_INP_TP', 'AUD_INP_LN', 'AUD_PR']:
+        datestamp = get_date()
+        timestamp = date_to_timestamp(datestamp)
+        USER_DATA[msg.from_user.id]['recording'] = {'date': datestamp, 'timestamp': timestamp, 'topic': 'None'}
 
-    voice_message = await bot.get_file(msg.voice.file_id)
-    await bot.download_file(voice_message.file_path, f'{filename}.ogg')
+        voice_message = await bot.get_file(msg.voice.file_id)
+        await bot.download_file(voice_message.file_path, f'{msg.chat.id}_{timestamp}.ogg')
 
-    process = subprocess.Popen(['ffmpeg', '-i', f'{filename}.ogg', f'{filename}.wav'], shell=True)
-    if process.returncode != 0:
-        raise Exception('Something went wrong')   # FIXME
+        USER_DATA[msg.from_user.id]['state'] = 'AUD_INP_TP'
+        await msg.reply(f'Please enter the topic for this recording.', reply_markup=freq_used_topics_kb)
 
-    user_text = recognize_phrase(VOICE_MODEL, f'{filename}.wav')
-    await db_controller.create_recording(user_id=msg.from_user.id, recording={
-        'date': datestamp,
-        'timestamp': timestamp,
-        'topic': 'None',   # TODO ADD TOPICS
-        'text': user_text
-    })
 
-    await msg.reply(f'Message stored: {datestamp}', reply_markup=get_rec_kb)
-
-    os.remove(f'{filename}.ogg')
-    os.remove(f'{filename}.wav')
+async def convert_voice_message(msg: types.Message):
+    filename = f'{msg.from_user.id}_{USER_DATA[msg.from_user.id]["recording"]["timestamp"]}'
+    try:
+        # convert ogg to wav using ffmpeg
+        subprocess.Popen(['ffmpeg', '-i', f'{filename}.ogg', f'{filename}.wav'], shell=True)
+        # wait before it updates the list of files
+        time.sleep(0.5)
+        USER_DATA[msg.from_user.id]['recording']['text'] = process_text(
+            recognize_phrase(f'{filename}.wav', language=USER_DATA[msg.from_user.id]['recording']['language'])
+        )
+        await db_controller.create_recording(user_id=msg.from_user.id,
+                                             recording=USER_DATA[msg.from_user.id]['recording'])
+    except FileNotFoundError:
+        print('SERVER ERROR')
+    else:
+        await msg.reply(f'Message stored: {USER_DATA[msg.from_user.id]["recording"]["date"]}', reply_markup=get_rec_kb)
+    finally:
+        os.remove(f'{filename}.ogg')
+        os.remove(f'{filename}.wav')
+        print(USER_DATA)
+        USER_DATA[msg.from_user.id].pop('recording')
+        USER_DATA[msg.from_user.id]['state'] = 'IDLE'
 
 
 if __name__ == '__main__':
